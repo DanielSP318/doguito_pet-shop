6cfaafc0fe049b7a84f39597593819779e0737c6
Daniel Silva Pereira <93915390+DanielSP318@users.noreply.github.com>
2022-06-29 17:31:30 -0300

    Projeto pronto

---
+++ src/paginas/Page404.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import image from '../assets/img/doguito404.svg'
+import '../assets/css/404.css'
+
+const Page404 = () => {
+    return(
+        <main className='container flex flex--centro flex--coluna'>
+            <img className='doguito-imagem' src={image} alt='ilutração do doguito' />
+            <p className='naoencontrado-texto'>
+                Ops, essa página não existe!
+            </p>
+        </main>
+    );
+}
+
+export default Page404;
